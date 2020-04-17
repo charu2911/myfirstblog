556f5c2508b963d943f44ebdaaa71380c185c8b6
charu2911 <51871883+charu2911@users.noreply.github.com>
2020-04-17 09:23:53 +0530

    Commit through WEB UI
    Commit through WEB UI

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bv+(72xd5po7y80p&^&0=-u!hsw=bu(24+%)yjij5b1!)vosym'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
